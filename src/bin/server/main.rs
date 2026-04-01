2fabc9066ad8163aae210b0495ba2d450ea60d55
チセ <123655015+chise0713@users.noreply.github.com>
2026-04-01 10:10:30 +0800

    Server: Use `Box<[Box<Path>]>` then `sort_unstable`

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -2,11 +2,11 @@ mod accept;
 mod args;
 mod path_table;
 
-use std::{collections::BTreeSet, num::NonZero, path::Path, process::ExitCode, sync::Arc, thread};
+use std::{num::NonZero, path::PathBuf, process::ExitCode, sync::Arc, thread};
 
 use anyhow::Result;
 use tokio::{net::TcpListener, runtime::Runtime, signal};
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 
 use crate::{
     accept::Accept,
@@ -30,22 +30,19 @@ fn main() -> Result<ExitCode> {
 
     let listen = std::net::TcpListener::bind(listen.as_ref())?;
 
-    // btree build stable file indexs
-    let mut set: BTreeSet<Box<Path>> = BTreeSet::new();
-
-    let walkdir = WalkDir::new(root.as_ref())
+    let mut paths: Box<[_]> = WalkDir::new(root.as_ref())
         .follow_root_links(false)
         .into_iter()
         .filter_map(Result::ok)
-        .filter(|e| e.path().is_file());
-    for entry in walkdir {
-        let path = entry.path();
-        set.insert(Box::from(path));
-    }
+        .map(DirEntry::into_path)
+        .map(PathBuf::into_boxed_path)
+        .filter(|p| p.is_file())
+        .collect();
+    paths.sort_unstable();
 
     let (rt, async_main) = AsyncMain::new(
         // freeze btree into boxed slice
-        PathTable::new(set)?,
+        PathTable::new(paths)?,
         listen,
     )?;
     rt.block_on(async_main.enter())
